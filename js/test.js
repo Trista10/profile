dedaabd2dab667256ed0a96c326947967c7e69fe
trista <allice203@gmail.com>
2019-07-17 17:14:49 +0800

    no message

--- js/test.js
+++ js/test.js
@@ -21,3 +21,22 @@ function testA(){
 }
 
 
+var boxB = document.getElementById("pure-formB").getElementsByTagName("input");
+var sumB = 0;
+
+function testB(){
+    for(var i=0;i<boxB.length;i++){
+        if(boxB[i].checked){
+            sumB += parseInt(boxB[i].value,10);
+        }
+    };
+    if(sumB >= 19 && sumB <= 32){
+        window.open("https://trista10.github.io/profile/lion.html");
+    }else if(sumB >= 33 && sumB <= 47){
+        window.open("https://trista10.github.io/profile/beer.html");
+    }else if(sumB >= 48 && sumB <= 61){
+        window.open("https://trista10.github.io/profile/wolf.html");
+    }else{
+        alert("請填寫完整才能透過測驗得知您的類型唷！");
+    }
+}
