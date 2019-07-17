54d22a77ef576e428e56d708a70905aa8d229040
trista <allice203@gmail.com>
2019-07-17 12:50:21 +0800

    no message

---
+++ js/all.js
@@ -0,0 +1,51 @@
+// 引用header、footer
+function templetes(){
+    
+    $.ajax({
+        url: '../templete/footer.html',
+        success: function(response){
+            $("#hep_footer").html(response);
+        }
+    });
+    
+    $.ajax({
+        url: '../templete/header.html',
+        success: function(response2){
+            $("#hep_header").html(response2);
+        }
+    });
+    
+    
+}
+
+
+
+// 手機 menu向下展開
+$(document).ready(function() {
+    $('.l-nav__showBtn').on('click',  function(e){
+    e.preventDefault();
+    $('.l-nav__link').toggleClass('l-link__showMenu');
+    });
+});
+
+
+// gototop 捲軸回上方
+$(document).ready(function() {
+    // 控制回top速度
+    $('.gototop').click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    // 判斷捲軸高度大於70px，則顯示gototop按鈕，反之移除
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 70 ){
+            $('.gototop').fadeIn(222);
+        } else {
+            $('.gototop').stop().fadeOut(222);
+        }
+    }).scroll();
+
+});
+
