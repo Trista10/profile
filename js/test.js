38beb7db8dc873cef1e2eeb26160d4f636baded6
trista <allice203@gmail.com>
2019-07-17 15:21:34 +0800

    window.location.replace

--- js/test.js
+++ js/test.js
@@ -13,9 +13,9 @@ for(var i=0;i<boxs.length;i++){
 
 function testA(){
     if(sum >=7 ){
-        window.location("http://www.google.com");
+        window.location.replace("http://www.google.com");
     }else{
-        window.location("http://www.yahoo.com.tw");
+        window.location.replace("http://www.yahoo.com.tw");
     }
 }
 
