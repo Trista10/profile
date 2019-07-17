49260e185d4d3a5a5c500afb4d133ce3f80f8724
trista <allice203@gmail.com>
2019-07-17 17:25:29 +0800

    no message

--- js/testB.js
+++ js/testB.js
@@ -1,26 +1,3 @@
-
-
-// if 設定值累積 >=7，則跳到dolphin.html，否則到testB.html
-
-var boxs = document.getElementById("pure-formA").getElementsByTagName("input");
-var sum = 0;
-
-function testA(){
-    for(var i=0;i<boxs.length;i++){
-        if(boxs[i].checked){
-            sum += parseInt(boxs[i].value,10);
-        }
-    };
-    if(sum >=7){
-        window.open("https://trista10.github.io/profile/dolphin.html");
-        // alert("大於>7");
-    }else{
-        window.open("https://trista10.github.io/profile/testB.html");
-        // alert("小於<7");
-    }
-}
-
-
 var boxB = document.getElementById("pure-formB").getElementsByTagName("input");
 var sumB = 0;
 
