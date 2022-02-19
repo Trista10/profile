0e7dbf20af85db6a8346b9894b1bcb7ee2bde504
Trista10 <allice203@gmail.com>
2022-02-19 17:54:09 +0800

    Update testB.js

--- profile01/js/testB.js
+++ profile01/js/testB.js
@@ -9,10 +9,13 @@ function testB(){
     };
     if(sumB >= 19 && sumB <= 32){
         window.open("https://trista10.github.io/profile01/profile01/lion.html");
+        // window.location("https://trista10.github.io/profile01/profile01/lion.html"); GITHUB無法使用
     }else if(sumB >= 33 && sumB <= 47){
         window.open("https://trista10.github.io/profile01/profile01/beer.html");
+        // window.location("https://trista10.github.io/profile01/profile01/beer.html"); GITHUB無法使用
     }else if(sumB >= 48 && sumB <= 61){
         window.open("https://trista10.github.io/profile01/profile01/wolf.html");
+        // window.location("https://trista10.github.io/profile01/profile01/wolf.html"); GITHUB無法使用
     }else{
         alert("請填寫完整才能透過測驗得知您的類型唷！");
     }
